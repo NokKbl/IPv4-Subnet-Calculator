ffdd5e08af8ccff0db8836cad4721b20f6e25158
Kornphon Noiprasert <konphon22@hotmail.com>
2018-12-06 14:47:51 +0700

    Update error

--- subcal/views.py
+++ subcal/views.py
@@ -13,98 +13,114 @@ def IndexView(request):
         number = request.POST.get('number')
         number_chceked = request.POST.get('number')
         radio = request.POST.get('option')
-        text_class = check_class(all_ip[0])
-        number = int(number)
-        number = check_expo(number)
-        number = net_host(number,radio,text_class)
+        try :
+            text_class = check_class(all_ip[0])
+            number = int(number)
+            number = check_expo(number)
+            number = net_host(number,radio,text_class)
 
-        array = []
-        for i in range(0, 32):
-            array.append(str(0))
-        array_new = change_binary_two(text_class, number, all_ip,array)
-        for_subnet = array.copy()
-        subnet_mask = change_binary(text_class, number, for_subnet) #replace 1 with fixed class and host
+            array = []
+            for i in range(0, 32):
+                array.append(str(0))
+            array_new = change_binary_two(text_class, number, all_ip,array)
+            for_subnet = array.copy()
+            subnet_mask = change_binary(text_class, number, for_subnet) #replace 1 with fixed class and host
 
-        five_line = fix_subnet_id(array_new,number,text_class)
-        
-        first_line = five_line[0]
-        second_line = five_line[1]
-        third_line = five_line[2]
-        last_line = five_line[3]
+            five_line = fix_subnet_id(array_new,number,text_class)
+            
+            first_line = five_line[0]
+            second_line = five_line[1]
+            third_line = five_line[2]
+            last_line = five_line[3]
 
-        fourth_line = five_line[4]
-        
-        add_first = first_add(first_line)
-        add_second = first_add(second_line)
-        add_third = first_add(third_line)
-        add_last = first_add(last_line)
+            fourth_line = five_line[4]
+            
+            add_first = first_add(first_line)
+            add_second = first_add(second_line)
+            add_third = first_add(third_line)
+            add_last = first_add(last_line)
 
-        broadcast_first = broad_cast(second_line)
-        broadcast_second = broad_cast(third_line)
-        broadcast_third = broad_cast(fourth_line)
-        broadcast_last = broad_cast_special(array_new)
+            broadcast_first = broad_cast(second_line)
+            broadcast_second = broad_cast(third_line)
+            broadcast_third = broad_cast(fourth_line)
+            broadcast_last = broad_cast_special(array_new)
 
-        last_first = broad_cast(broadcast_first)
-        last_second = broad_cast(broadcast_second)
-        last_third = broad_cast(broadcast_third)
-        last_last = broad_cast(broadcast_last)
+            last_first = broad_cast(broadcast_first)
+            last_second = broad_cast(broadcast_second)
+            last_third = broad_cast(broadcast_third)
+            last_last = broad_cast(broadcast_last)
 
-        first_line = float_to_int(first_line)
-        second_line = float_to_int(second_line)
-        third_line = float_to_int(third_line)
-        last_line = float_to_int(last_line)
-        
-        add_first = float_to_int(add_first)
-        add_second = float_to_int(add_second)
-        add_third = float_to_int(add_third)
-        add_last = float_to_int(add_last)
+            first_line = float_to_int(first_line)
+            second_line = float_to_int(second_line)
+            third_line = float_to_int(third_line)
+            last_line = float_to_int(last_line)
+            
+            add_first = float_to_int(add_first)
+            add_second = float_to_int(add_second)
+            add_third = float_to_int(add_third)
+            add_last = float_to_int(add_last)
 
-        broadcast_first = float_to_int(broadcast_first)
-        broadcast_second = float_to_int(broadcast_second)
-        broadcast_third = float_to_int(broadcast_third)
-        broadcast_last = float_to_int(broadcast_last)
+            broadcast_first = float_to_int(broadcast_first)
+            broadcast_second = float_to_int(broadcast_second)
+            broadcast_third = float_to_int(broadcast_third)
+            broadcast_last = float_to_int(broadcast_last)
 
-        last_first = float_to_int(last_first)
-        last_second = float_to_int(last_second)
-        last_third = float_to_int(last_third)
-        last_last = float_to_int(last_last)
+            last_first = float_to_int(last_first)
+            last_second = float_to_int(last_second)
+            last_third = float_to_int(last_third)
+            last_last = float_to_int(last_last)
 
-        first_line = four_int_to_string(first_line)
-        second_line = four_int_to_string(second_line)
-        third_line = four_int_to_string(third_line)
-        last_line = four_int_to_string(last_line)
-        
-        add_first = four_int_to_string(add_first)
-        add_second = four_int_to_string(add_second)
-        add_third = four_int_to_string(add_third)
-        add_last = four_int_to_string(add_last)
+            first_line = four_int_to_string(first_line)
+            second_line = four_int_to_string(second_line)
+            third_line = four_int_to_string(third_line)
+            last_line = four_int_to_string(last_line)
+            
+            add_first = four_int_to_string(add_first)
+            add_second = four_int_to_string(add_second)
+            add_third = four_int_to_string(add_third)
+            add_last = four_int_to_string(add_last)
 
-        broadcast_first = four_int_to_string(broadcast_first)
-        broadcast_second = four_int_to_string(broadcast_second)
-        broadcast_third = four_int_to_string(broadcast_third)
-        broadcast_last = four_int_to_string(broadcast_last)
+            broadcast_first = four_int_to_string(broadcast_first)
+            broadcast_second = four_int_to_string(broadcast_second)
+            broadcast_third = four_int_to_string(broadcast_third)
+            broadcast_last = four_int_to_string(broadcast_last)
 
-        last_first = four_int_to_string(last_first)
-        last_second = four_int_to_string(last_second)
-        last_third = four_int_to_string(last_third)
-        last_last = four_int_to_string(last_last)
+            last_first = four_int_to_string(last_first)
+            last_second = four_int_to_string(last_second)
+            last_third = four_int_to_string(last_third)
+            last_last = four_int_to_string(last_last)
 
-        first = ["0",first_line,add_first,last_first,broadcast_first]
-        second = ["1",second_line,add_second,last_second,broadcast_second]
-        third = ["2",third_line,add_third,last_third,broadcast_third]
-        fourth = [".",".",".",".","."]
-        fifth = [".",".",".",".","."]
-        last = [str(int(pow(2,number))-1),last_line,add_last,last_last,broadcast_last]
-        
-        subresult = [first,second,third,fourth,fifth,last]
-        
-        ip_address_string = four_int_to_string(float_to_int(first_add(broad_cast(array_new))))
-        mask =  four_int_to_string(float_to_int(first_add(broad_cast(subnet_mask))))
-        if text_class == "Class D" or text_class == "Class E":
-            mask = "No mask"
+            first = ["0",first_line,add_first,last_first,broadcast_first]
+            second = ["1",second_line,add_second,last_second,broadcast_second]
+            third = ["2",third_line,add_third,last_third,broadcast_third]
+            fourth = [".",".",".",".","."]
+            fifth = [".",".",".",".","."]
+            last = [str(int(pow(2,number))-1),last_line,add_last,last_last,broadcast_last]
+            
+            subresult = [first,second,third,fourth,fifth,last]
+            
+            ip_address_string = four_int_to_string(float_to_int(first_add(broad_cast(array_new))))
+            mask =  four_int_to_string(float_to_int(first_add(broad_cast(subnet_mask))))
+            if text_class == "Class D" or text_class == "Class E":
+                mask = "No mask"
+                subresult = []
+            if int(number_chceked) <= 2 :
+                subresult = [first,second]
+            if text_class == "Impossible!!" :
+                mask = "-"
+                ip_address_string = str(ip)
+                subresult = []
+            if int(all_ip[0]) > 255 or int(all_ip[1]) > 255 or int(all_ip[2]) > 255 or int(all_ip[3]) > 255:
+                subresult = []
+                ip_address_string = "Impossible IP Address!!"
+                text_class = "Impossible Class!!"
+                mask = "No mask"
+        except ValueError:
             subresult = []
-        if int(number_chceked) <= 2 :
-            subresult = [first,second]
+            ip_address_string = "Not an IP Address!!"
+            text_class = "-"
+            mask = "-"
+
     context = {
         'subresult' : subresult,
         'ip_add' : ip_address_string,
